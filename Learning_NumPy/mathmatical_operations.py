65e59febc8e49c9260e4a4897876488780a6ef5d
GioGioWasTaken <112432039+GioGioWasTaken@users.noreply.github.com>
2023-01-24 14:10:04 +0200

    got a first classification model working

---
+++ Learning_NumPy/mathmatical_operations.py
@@ -0,0 +1,5 @@
+import numpy as np
+a=np.arange(6)
+print(a)
+a.reshape(-1,2)
+print(a)
---
+++ pycharm_ML/one_pred_LR.py
@@ -0,0 +1,12 @@
+import numpy as np
+X_train=np.array([2104,5,1,45])
+y_train=np.array([460])
+w_init=np.array([0.39 ,18.75,-53.36,-26.42])
+b_init=785.1811367994083
+def prediction(X,w,b):
+    n=X.shape[0]
+    for i in range(n):
+        p=np.dot(X,w)+b
+    return p
+
+print(prediction(X_train,w_init,b_init))
